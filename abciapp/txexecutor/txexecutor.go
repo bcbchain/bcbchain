10105553b529f4501c73688dba20be6468e985ef
Big-Aaron <lixino727@qq.com>
2020-11-24 12:32:56 +0800

    跨链交易串行执行

--- abciapp/txexecutor/txexecutor.go
+++ abciapp/txexecutor/txexecutor.go
@@ -1,16 +1,23 @@
 package txexecutor
 
 import (
+	"fmt"
 	deliverV2 "github.com/bcbchain/bcbchain/abciapp/service/deliver"
 	"github.com/bcbchain/bcbchain/abciapp/txpool"
 	"github.com/bcbchain/bcbchain/abciapp_v1.0/contract/stubapi"
 	deliverV1 "github.com/bcbchain/bcbchain/abciapp_v1.0/service/deliver"
 	"github.com/bcbchain/bcbchain/common/statedbhelper"
 	"github.com/bcbchain/bcbchain/statedb"
+	"github.com/bcbchain/bclib/algorithm"
 	"github.com/bcbchain/bclib/tendermint/abci/types"
 	"github.com/bcbchain/bclib/tendermint/tmlibs/log"
 	types2 "github.com/bcbchain/bclib/types"
 	"runtime"
+	"strings"
+)
+
+const (
+	genesisOrgID = "orgJgaGConUyK81zibntUBjQ33PKctpk1K1G"
 )
 
 type TxExecutor interface {
@@ -69,16 +76,46 @@ func (te *txExecutor) execRoutine() {
 		execTxs := te.txPool.GetTxsExecPending() //获取一批解析好后的交易，数量不确定
 
 		if te.haveV1Transaction(execTxs) == true {
-			// 存在v1版本交易时，当前分片按照串行方式执行
+			// 存在v1版本交易,当前分片按照串行方式执行
+			for _, execTx := range execTxs {
+				tempExecTxs := make([]*statedb.Tx, 0)
+				tempExecTxs = append(tempExecTxs, execTx)
+				te.transaction.GoBatchExec(tempExecTxs)
+
+				parseTx := te.txPool.GetParseTx(te.handleTxsNum)
+				te.handleTxsNum++
+				var connV2 *deliverV2.AppDeliver
+				if txpool.ChainVerison == 2 {
+					connV2 = te.deliverAppV2
+				}
+				resDeliverTx := te.deliverAppV1.HandleResponse(
+					execTx,
+					parseTx.TxStr(),
+					parseTx.RawTxV1(),
+					execTx.Response().(stubapi.Response),
+					connV2,
+				)
+				resDeliverTx.TxHash = algorithm.CalcCodeHash(parseTx.TxStr())
+				te.logger.Info("Send ABCI Response: execRoutine", "txID", execTx.ID(), "txHash", resDeliverTx.TxHash.String())
+				te.responsesChan <- resDeliverTx
+
+			}
+		} else if te.haveSideChainTx(execTxs) == true {
+			// 存在v2版本的跨链交易时，当前分片按照串行方式执行
 			for _, execTx := range execTxs {
 				tempExecTxs := make([]*statedb.Tx, 0)
 				tempExecTxs = append(tempExecTxs, execTx)
 				te.transaction.GoBatchExec(tempExecTxs)
 			}
 		} else {
-			te.transaction.GoBatchExec(execTxs) //进入数据库中执行tx所带的执行函数
+			te.transaction.GoBatchExec(execTxs) //并发执行tx所带的执行函数
 		}
 
+		//for _, execTx := range execTxs {
+		//	tempExecTxs := make([]*statedb.Tx, 0)
+		//	tempExecTxs = append(tempExecTxs, execTx)
+		//	te.transaction.GoBatchExec(tempExecTxs)
+		//}
 		for _, execTx := range execTxs {
 			parseTx := te.txPool.GetParseTx(te.handleTxsNum)
 			te.handleTxsNum++
@@ -94,6 +131,8 @@ func (te *txExecutor) execRoutine() {
 					execTx.Response().(stubapi.Response),
 					connV2,
 				)
+				resDeliverTx.TxHash = algorithm.CalcCodeHash(parseTx.TxStr())
+				te.logger.Info("Send ABCI Response: execRoutine", "txID", execTx.ID(), "txHash", resDeliverTx.TxHash.String())
 				te.responsesChan <- resDeliverTx
 			} else if parseTx.RawTxV2() != nil {
 				resDeliverTx := te.deliverAppV2.HandleResponse(
@@ -102,6 +141,8 @@ func (te *txExecutor) execRoutine() {
 					parseTx.RawTxV2(),
 					execTx.Response().(*types2.Response),
 				)
+				resDeliverTx.TxHash = algorithm.CalcCodeHash(parseTx.TxStr())
+				te.logger.Info("Send ABCI Response: execRoutine", "txID", execTx.ID(), "txHash", resDeliverTx.TxHash.String())
 				te.responsesChan <- resDeliverTx
 			}
 		}
@@ -128,9 +169,24 @@ func (te *txExecutor) TENET() {
 func (te *txExecutor) haveV1Transaction(execTxs []*statedb.Tx) bool {
 	tempHandleTxsNum := te.handleTxsNum
 	for range execTxs {
-		parseTx := te.txPool.GetParseTx(te.handleTxsNum)
+		parseTx := te.txPool.GetParseTx(tempHandleTxsNum)
 		tempHandleTxsNum++
 		if parseTx.RawTxV1() != nil {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (te *txExecutor) haveSideChainTx(execTxs []*statedb.Tx) bool {
+	tempHandleTxsNum := te.handleTxsNum
+	contractAddr := statedbhelper.GetContractsByName(0, 0, "ibc", genesisOrgID)
+	for range execTxs {
+		parseTx := te.txPool.GetParseTx(tempHandleTxsNum)
+		tempHandleTxsNum++
+		messagesContract := strings.Split(fmt.Sprint(parseTx.RawTxV2().Messages[0].Contract), ".")
+		if parseTx.RawTxV2() != nil && messagesContract[0] == genesisOrgID && messagesContract[len(messagesContract)-1] == contractAddr[len(contractAddr)-1] {
 			return true
 		}
 	}
